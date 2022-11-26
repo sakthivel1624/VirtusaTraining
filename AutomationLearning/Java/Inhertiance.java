7ecf57925f0148f60563a0ce1910a930134d18c2
sakthivel1624 <113850052+sakthivel1624@users.noreply.github.com>
2022-11-26 13:57:54 +0530

    Update Inhertiance.java

--- AutomationLearning/Java/Inhertiance.java
+++ AutomationLearning/Java/Inhertiance.java
@@ -1,1 +1,52 @@
 
+package inheritance;
+
+class School {
+
+private String name;
+
+School(String name) {
+
+this.name = name;
+
+}
+
+public String SchoolName() {
+
+return this.name;
+
+}
+
+}
+
+class Student {
+
+private String name;
+
+Student(String name) {
+
+this.name = name;
+
+}
+
+public String StudentName() {
+
+return this.name;
+
+}
+
+}
+
+public class Drive {
+
+public static void main(String[] args) {
+
+School schl = new School("St.John's School");
+
+Student candidate = new Student("Tobey Marshall");
+
+System.out.println(candidate.StudentName() + " is an Ex-Student of " + schl.SchoolName());
+
+}
+
+}
