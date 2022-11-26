749af608b331f19da7c686efe6728373cb604c45
sakthivel1624 <113850052+sakthivel1624@users.noreply.github.com>
2022-11-26 09:47:26 +0530

    Update and rename Alerthandle.txt to TextEXtraction.java

--- AutomationLearning/Selenium/TextEXtraction.java
+++ AutomationLearning/Selenium/TextEXtraction.java
@@ -14,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AlertHandle {
 
-	private static WebDriver driver;
+	public static WebDriver driver;
 
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
@@ -23,17 +23,7 @@ public class AlertHandle {
 		driver.get(baselink);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-		/*
-		 * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); WebElement
-		 * we = driver.findElement(By.xpath("//*[@id=\"ta1\"]")); we.click();
-		 * //*[@id="Blog1"]/div[1]/div/div/div/div/h3/a we.sendKeys("hello");
-		 * 
-		 * WebElement we = driver.findElement(By.xpath("")); we.click(); Alert
-		 * alertwindow = driver.switchTo().alert(); alertwindow.accept(); String text =
-		 * alertwindow.getText(); System.out.println(text);
-		 */
 		WebElement fi = driver.findElement(By.xpath("(//h2[contains(text(),'Other Sites to Practice Automation')])"));
-        System.out.println(fi.getText());
-		  
+                System.out.println(fi.getText());	  
 	}
 }
