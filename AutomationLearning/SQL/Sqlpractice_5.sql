cf019c5c40893ea62f0976a89649e74bd02f5c7e
sakthivel1624 <113850052+sakthivel1624@users.noreply.github.com>
2022-11-27 09:13:03 +0530

    Delete Sqlpractice_5.sql

--- AutomationLearning/SQL/Sqlpractice_5.sql
+++
@@ -1,7 +0,0 @@
---Primary Key Demonstartion
-CREATE TABLE purchase_orders (
-    po_nr NUMBER PRIMARY KEY,
-    vendor_id NUMBER NOT NULL,
-    po_status NUMBER(1,0) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL 
-);
